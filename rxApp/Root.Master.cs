2b9ed86f646e000a2a67a59c086800a31745a8a7
Renato Xavier <you@example.com>
2022-07-26 22:33:16 -0300

    deploy layout fixes vinicinho

--- rxApp/Root.Master.cs
+++ rxApp/Root.Master.cs
@@ -9,5 +9,17 @@ namespace rxApp
         {
             ASPxMenu1.Visible = Page.User?.Identity.IsAuthenticated == true;
         }
+
+        protected void ASPxMenu1_ItemDataBound(object source, DevExpress.Web.MenuItemEventArgs e)
+        {
+            if (e.Item.Depth == 0)
+            {
+                e.Item.ItemStyle.ForeColor = System.Drawing.Color.White;
+            } else if (e.Item.Depth == 1)
+            {
+                e.Item.ItemStyle.ForeColor = System.Drawing.Color.Black;
+                e.Item.ItemStyle.HoverStyle.ForeColor = System.Drawing.ColorTranslator.FromHtml("#DEDFDA");
+            }
+      }
     }
 }
