bee17e717d36cd97f1508e14fa847d0b2d2a1be4
Renato Xavier <renatoferreiraxavier@gmail.com>
2023-02-01 10:12:26 -0300

    grid ack get info

--- rxApp/Models/IdentityModels.cs
+++ rxApp/Models/IdentityModels.cs
@@ -54,6 +54,7 @@ namespace rxApp.Models
         public DbSet<GetLockLojaClienteRedeView> GetLockLojaClienteRedeViews { get; set; }
         public DbSet<GetLockMessageAckGetStatus> GetLockMessageAckGetStatuses { get; set; }
         public DbSet<GetLockMessageAckDevLock> GetLockMessageAckDevLocks { get; set; }
+        public DbSet<GetLockMessageAckGetInfo> GetLockMessageAckGetInfos { get; set; }
         public DbSet<GetLockCofreCadastroView> GetLockCofreCadastroViews { get; set; }
 
 
@@ -99,8 +100,8 @@ namespace rxApp.Models
             modelBuilder.Configurations.Add(new GetLockLojaClienteRedeViewConfig());
             modelBuilder.Configurations.Add(new GetLockMessageAckGetStatusConfig());
             modelBuilder.Configurations.Add(new GetLockMessageAckDevLockConfig());
+            modelBuilder.Configurations.Add(new GetLockMessageAckGetInfoConfig());
             modelBuilder.Configurations.Add(new GetLockCofreCadastroViewConfig());
-
 
             base.OnModelCreating(modelBuilder);
         }
